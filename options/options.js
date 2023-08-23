bc8eca2804658a4bd7c95255c1ab23470445020c
Crowdbotics <engineering@crowdbotics.com>
2023-08-23 00:50:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-lake-dev-110643",
+  url: "https://royal-lake-dev-110643.botics.co",
+  api: "https://royal-lake-dev-110643.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
